9dff1a0ee7a2e8b76349b2ca047f4ac96c041513
retuci0 <retucio@gmail.com>
2026-02-11 21:34:34 +0100

    1.1.0

--- src/main/java/com/github/retucio/neutrino/Event.java
+++ src/main/java/com/github/retucio/neutrino/Event.java
@@ -3,6 +3,7 @@ package com.github.retucio.neutrino;
 public class Event {
 
     private boolean cancelled = false;
+    private Stage stage = Stage.PRE;
 
     public boolean isCancelled() {
         return cancelled;
@@ -10,5 +11,18 @@ public class Event {
 
     public void cancel() {
         cancelled = true;
+    }
+
+    public Stage getStage() {
+        return stage;
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public enum Stage {
+        PRE,
+        POST;
     }
 }
